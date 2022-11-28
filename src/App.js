ccc6fdabead9e37e72a5c903168a0dcaf67db47b
Ofego Edafe <ofegoedafe@Ofegos-MacBook-Pro.local>
2022-11-28 21:23:42 +1100

    Fetch Api from endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import cogoToast from 'cogo-toast';
+
+
 function App() {
+
+  const [tableData, setTableData] = useState([])
+
+
+  const fetchApi = async () => {
+    try {
+      console.log("working")
+      fetch("http://universities.hipolabs.com/search?country=Australia")
+        .then(response => response.text())
+        .then(result => {
+          cogoToast.success("Data loaded successfully")
+          return setTableData(JSON.parse(result))
+        })
+        .catch(error => console.log('error', error));
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  const onLoad = () => {
+    try {
+      fetchApi()
+    } catch (error) {
+      console.log("Error", error)
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
